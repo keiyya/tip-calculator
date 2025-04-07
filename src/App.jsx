23ef2af56c8eba65c3cefef85a36ed670e515f4e
Keiyya Hassan <keiyyahassan@Keiyyas-MacBook-Pro.local>
2025-04-07 17:19:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-green">
+        Hello React <span className="text-green-400">Tailwind</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
